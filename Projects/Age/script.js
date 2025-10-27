7b5d25bb1956cb76a3f8c5048bc494a32ac06ceb
Peter Coker <petercoker@protonmail.com>
2025-10-27 12:05:48 +0000

    feat(added Todo to error handle):

--- Projects/Age/script.js
+++ Projects/Age/script.js
@@ -2,3 +2,5 @@ let myAge = prompt("Enter my age:");
 let yourAge = prompt("Please enter your age:");
 
 alert(`I am ${myAge} years old\nYou are ${yourAge} years old`);
+
+//TODO : Error to make sure the user enter a number not a string
