fd453fe108982150cfb14a9878afeefdeb5ac11e
Peter Coker <petercoker@protonmail.com>
2025-10-27 12:03:07 +0000

    fix(used /n instead /t to output the text to next line intead):

--- Exercises/Day_1/Day_1.js
+++ Exercises/Day_1/Day_1.js
@@ -65,4 +65,4 @@ let yourAge = 30;
 console.log(`I am ${myAge} years old.`);
 console.log(`You are ${yourAge} years old`);
 
-console.log(`I am ${myAge} years old\tYou are ${yourAge} years old`);
+console.log(`I am ${myAge} years old\nYou are ${yourAge} years old`);
---
+++ Projects/Age/script.js
@@ -0,0 +1,4 @@
+let myAge = prompt("Enter my age:");
+let yourAge = prompt("Please enter your age:");
+
+alert(`I am ${myAge} years old\nYou are ${yourAge} years old`);
