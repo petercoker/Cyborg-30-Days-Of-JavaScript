3917b32e22916ab1d84e8771ed84ed5538d12b56
Peter Coker <petercoker@protonmail.com>
2025-11-15 18:42:11 +0100

    level 3 complete

--- Exercises/Day_3/level_3.js
+++ Exercises/Day_3/level_3.js
@@ -1,2 +1,15 @@
 // 1. Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
 // YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+
+const date = new Date();
+
+const options = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
+};
+
+console.log(date.toLocaleString("en-CA", options));
