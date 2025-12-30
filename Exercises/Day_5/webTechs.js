002e7d679685650be47c2ebf1cb31b602aff8603
Peter Coker <petercoker@protonmail.com>
2025-12-30 14:20:38 +0100

    complete level 2 exericese 1 and 2

---
+++ Exercises/Day_5/webTechs.js
@@ -0,0 +1,39 @@
+// const webTechs = [
+//   "HTML",
+//   "CSS",
+//   "JavaScript",
+//   "TypeScript",
+//   "React",
+//   "Redux",
+//   "Node.js",
+//   "Express",
+//   "MongoDB",
+//   "GraphQL",
+//   "Webpack",
+//   "Babel",
+//   "Sass",
+//   "Tailwind CSS",
+//   "Docker",s
+// ];
+
+// wrong
+// module.exports = webTechs;
+
+// Best practice
+export const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Redux",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "GraphQL",
+  "Webpack",
+  "Babel",
+  "Sass",
+  "Tailwind CSS",
+  "Docker",
+];
